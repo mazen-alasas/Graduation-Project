5aaa1947225ca42c60b24dcb3d7c4e37b5a4e3e6
esraa610 <esraashatat9@gmail.com>
2025-01-31 13:39:41 +0200

    UART
    UART_driver

---
+++ Embedded Systems/MCAL/UART/UART_Int.h
@@ -0,0 +1,15 @@
+#ifndef UART_INT_H
+#define UART_INT_H
+
+#include "../../StdTypes.h"
+#include "../../MemMap.h"
+#include "../../CFG/UART_Cfg.h"
+
+
+#define MYUBRR (F_CPU / 16 / BAUD - 1)
+
+void UART_INIT(void);
+void UART_Transmit(u8 data);
+u8 UART_Read(void);
+
+#endif /* UART_INT_H */

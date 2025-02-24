c5a06037565b75126ec2f44ecbe12cfb7aa12f92
hajar <105795305+Ghumball@users.noreply.github.com>
2025-02-24 16:37:41 +0200

    Delete helper.py

--- helper.py
+++
@@ -1,9 +0,0 @@
-from deepface import DeepFace
-
-def verify_face(self, frame):
-        try:
-            result = DeepFace.verify(img1_path=self.known_img, img2_path=frame)
-            return result["verified"]
-        except Exception:
-            print(f"Error during verification")
-            return False
